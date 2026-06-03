a5841c67210a6419b024174807f13239eff17899
Chirag Sharma <shamraaditya733@gmail.com>
2026-06-03 11:00:55 +0530

    Replying feature

--- src/main/java/com/test/Repository/friendRequestRepository.java
+++ src/main/java/com/test/Repository/friendRequestRepository.java
@@ -3,12 +3,11 @@ package com.test.Repository;
 import com.test.model.FriendRequest;
 import com.test.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface friendRequestRepository extends JpaRepository<FriendRequest , Long> {
