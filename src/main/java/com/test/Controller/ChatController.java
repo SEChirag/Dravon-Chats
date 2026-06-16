d0319b0d1257b9119bd3828db7626397c4d45500
Chirag Sharma <shamraaditya733@gmail.com>
2026-06-16 11:03:34 +0530

    unread

--- src/main/java/com/test/Controller/ChatController.java
+++ src/main/java/com/test/Controller/ChatController.java
@@ -243,13 +243,6 @@ public List<String> sendRequest(
         chatService.markRoomAsSeen(room , email);
         return ResponseEntity.ok("seen");
     }
-
-//    @GetMapping("/messages/{room}/debug")
-//    public ResponseEntity<Long> getUnreadCount(@PathVariable String room , @RequestHeader("Authorization") String authHeader) {
-//        String email = jwtUtil.extractEmail(authHeader.substring(7));
-//
-//        return ResponseEntity.ok(chatService.getUnreadCount(room , email));
-//    }
 @GetMapping("/messages/{room}/unread")
 public ResponseEntity<Long> getUnreadCount(@PathVariable String room , @RequestHeader("Authorization") String authHeader) {
 
